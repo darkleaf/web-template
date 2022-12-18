12c3421a03854e6400eb5a291a786aa282866976
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-12-18 18:34:09 +0400

    renderable test

--- test/darkleaf/web_template/impl/renderable_test.clj
+++ test/darkleaf/web_template/impl/renderable_test.clj
@@ -4,32 +4,28 @@
    [clojure.test :as t]))
 
 (t/deftest nil-test
-  (let [value nil
-        tmpl  (wt/compile ~value)]
-    (t/is (= "" (wt/render-to-string tmpl nil)))))
+  (let [renderable nil]
+    (t/is (= "" (wt/render-to-string renderable nil)))))
 
 (t/deftest object-test
-  (let [value (reify Object
-                (toString [_]
-                  "obj"))
-        tmpl (wt/compile ~value)]
-    (t/is (= "obj" (wt/render-to-string tmpl nil)))))
+  (let [renderable (reify Object
+                     (toString [_]
+                       "obj"))]
+    (t/is (= "obj" (wt/render-to-string renderable nil)))))
 
 (t/deftest fn-test
-  (let [value (fn [ctx]
-                (t/is (= ::data ctx))
-                "stub")
-        tmpl  (wt/compile ~value)
+  (let [renderable (fn [ctx]
+                    (t/is (= ::data ctx))
+                    "stub")
         data  ::data]
-    (t/is (= "stub" (wt/render-to-string tmpl data)))))
+    (t/is (= "stub" (wt/render-to-string renderable data)))))
 
 (def part "part")
 
 (t/deftest var-test
-  (let [tmpl (wt/compile #'part)]
-    (do
-      (t/is (= "part"
-               (wt/render-to-string tmpl nil))))
-    (with-redefs [part "redefined"]
-      (t/is (= "redefined"
-               (wt/render-to-string tmpl nil))))))
+  (do
+    (t/is (= "part"
+             (wt/render-to-string #'part nil))))
+  (with-redefs [part "redefined"]
+    (t/is (= "redefined"
+             (wt/render-to-string #'part nil)))))
