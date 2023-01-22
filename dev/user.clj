c6c2bc6119f275c8818810bbd6d5f4911b34514e
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2023-01-22 20:18:40 +0400

    add criterium [skip ci]

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,15 @@
 (ns user)
 
+(comment
+  (require '[criterium.core :as c])
+
+  (let [m {:a 1}]
+    (c/quick-bench
+     (get m :a))) ; Execution time mean : 4,475954 ns
+
+  (let [m {'a 1}]
+    (c/quick-bench
+     (get m 'a)))) ; Execution time mean : 7,571531 ns
 
 (comment
   (require '[nextjournal.clerk :as clerk])
